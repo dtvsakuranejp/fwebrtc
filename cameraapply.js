f88341812a820f938361b8240f1d1badd632ea34
dtvsakuranejp <68037108+dtvsakuranejp@users.noreply.github.com>
2020-07-15 11:42:23 +0900

    Add files via upload

--- cameraapply.js
+++ cameraapply.js
@@ -1,4 +1,5 @@
 const brightnessSlider = document.getElementById('brightness');
+const brightnessNumber = document.getElementById('brightnessNumber');
 const sceneModeSelect = document.getElementById('sceneMode');
 const whiteBalanceSelect = document.getElementById('whiteBalance');
 const focusModeSelect = document.getElementById('focusMode');
@@ -20,27 +21,34 @@ function removeApplyCameraEvent() {
 
 //streamに対してカメラ設定できるようにする
 function applyCameraSettings() {
-    const track = localstream.getVideoTracks()[0];//localstreamが未定義だと失敗する
-
+    const track = localStream.getVideoTracks()[0];//localstreamが未定義だと失敗する
     getCameraSettings(track);
-    brightnessSlider.oninput = applyBrightness(track, brightnessSlider.value);
-    updateCameraStatusTimer = setInterval(getCameraSettings(localstream),500)    
+
+    //イベントリスナ追加
+    const capabilities = track.getCapabilities();
+    if ('brightness' in capabilities) {
+        brightnessSlider.oninput = applyBrightness(track, brightnessSlider.value);
+//    updateCameraStatusTimer = setInterval(getCameraSettings(localStream),500)    
+    }
 }
 
 //streamの現在をカメラ設定に反映する
 function getCameraSettings(track) {
+    console.log(track);
     const capabilities = track.getCapabilities();
     const settings = track.getSettings();
   
     //明るさが有効かどうか判定
+    console.log(capabilities);
     if (!('brightness' in capabilities)) {
         brightnessSlider.hidden = true;
-        return;
+        brightnessSlider.textContent='使えません';
     } else {
         brightnessSlider.min = capabilities.brightness.min;
         brightnessSlider.max = capabilities.brightness.max;
         brightnessSlider.step = capabilities.brightness.step;
         brightnessSlider.value = settings.brightness;
+        brightnessSlider.textContent = '使えます'+settings.brightness;
         brightnessSlider.hidden = false;
     }
 }
